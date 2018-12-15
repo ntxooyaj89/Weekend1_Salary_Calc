2f1141343d137cf74f60600e29c29120e4133718
Tang Yang <ty4081097@gmail.com>
2018-12-14 18:19:18 -0600

    collect value from input

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 $(document).ready(readyNow);
 
-class Employees{
-    constructor(name, lastName, id, title, annualSalary){
+class Employees {
+    constructor(name, lastName, id, title, annualSalary) {
         this.name = name;
         this.lastName = lastName;
         this.id = id;
@@ -9,46 +9,48 @@ class Employees{
         this.annualSalary = annualSalary
 
     }
-} 
+}
 
-const jen = new Employees('Jen', 'Barber', '4521', 'team Lead', '80000')
-const maurice = new Employees('Maurice', 'Moss', '8724', 'Support Team', '58000')
-const roy = new Employees('Roy', 'Smith', '9623', 'Quality Assurance', '48000')
+const employees = [];
 
-const employees = [jen, maurice, roy]
-
-function readyNow(){
+function readyNow() {
     console.log('JQ');
     $('#submitBtn').on('click', addEmployee);
 }
 
-for(let i = 0; i < employees.length; i++){
+function addEmployee() {
+
+    const collectEmp = new Employees(
+        $('#nameIn').val(),
+        $('#lastNameIn').val(),
+        $('#idIn').val(),
+        $('#titleIn').val(),
+        $('#annualSalaryIn').val(),
+    )
+    employees.push(collectEmp);
+    console.log('this is employees', collectEmp);
+    updateEmployees;
+}
+
+function updateEmployees(){
+
+    
+}
+
+
+    // $('#empAppend').append(`<td>${name}</td>`);
+    // $('#empAppend').append(`<td>${lastName}</td>`);
+    // $('#empAppend').append(`<td>${id}</td>`);
+    // $('#empAppend').append(`<td>${title}</td>`);
+    // $('#empAppend').append(`<td>${salary}</td>`);
+
+
+
+
+
+
+
+
+for (let i = 0; i < employees.length; i++) {
     console.log(employees[i]);
 }
-
-function addEmployee(){
-    let name = $('#nameIn').val();
-    console.log(name);
-    let lastName = $('#lastNameIn').val();
-    console.log(lastName)
-    let id = $('#idIn').val();
-    console.log(id)
-    let title = $('#titleIn').val();
-    console.log(title)
-    let salary = $('#annualSalaryIn').val();
-    console.log(salary)
-
-    $('#empAppend').append(`<td>${name}</td>`);
-    $('#empAppend').append(`<td>${lastName}</td>`);
-    $('#empAppend').append(`<td>${id}</td>`);
-    $('#empAppend').append(`<td>${title}</td>`);
-    $('#empAppend').append(`<td>${salary}</td>`);
-
-
-    
-    
-}
-
-
-
-// $('#empAppend').append(`<tr>${employees[i].name}</tr>`)
