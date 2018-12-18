50c065d44bd90db1eaf9489612a0c46f34e41ff9
Tang Yang <ty4081097@gmail.com>
2018-12-18 12:46:12 -0600

    add delete th to table.

--- client.js
+++ client.js
@@ -50,6 +50,8 @@ function updateEmployees(arr) {
     // this loop through the array and append the employees name to DOM
     for (let i = 0; i < arr.length; i++) {
         console.log('this is update employees', arr[i]);
+        // everything that comes out of an inputs is a string. 
+        // parsenIn() it to turn into interger.
         let employeesName = `<tr>
          <td>${arr[i].name}</td>,
          <td>${arr[i].lastName}</td>, 
@@ -73,6 +75,7 @@ function updateEmployees(arr) {
 function costCal(total) {
 
     // this calculate the monthly cost and append to DOM
+    // 12 can be set as a global vairable and use here.
     let totalCost = total / 12;
     $('#costIn').html('<h4>Monthly total: ' + ' $ ' + Number(totalCost.toFixed(2)) + '</h4>');
     if (totalCost > 20000) {
@@ -90,7 +93,11 @@ function deleteEmp() {
     // "this" is now on the delete button inside the table
     // and it delete the parent().parent() which is the <td></td> 
     // where the employees names etc... lives
-    $(this).parent().parent().remove();
+
+    // * let deleteEmployee = $(this).parent().prev().prev().text();
+    // console.log('this is deleteEmployee', deleteEmployee);
+    // costTotal -= parseInt(deleteEmployee) / 12; //* 
+    $(this).parent().parent().remove(); // this remove the tr... 
 }
 
 
